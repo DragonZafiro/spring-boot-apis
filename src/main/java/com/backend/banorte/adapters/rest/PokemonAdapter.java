d2530fcdd4f91dc60e9953c03f89dfd8087e30d1
DanPC <dariuz44rzor@gmail.com>
2025-02-11 19:40:31 -0600

    se corrige Appconfig, se agrega una prueba Mock

--- src/main/java/com/backend/banorte/adapters/rest/PokemonAdapter.java
+++ src/main/java/com/backend/banorte/adapters/rest/PokemonAdapter.java
@@ -1,8 +1,8 @@
 package com.backend.banorte.adapters.rest;
-import com.backend.banorte.config.PokeApiUrl;
 import com.backend.banorte.domain.dto.pokemon.Pokemon;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -13,13 +13,13 @@ public class PokemonAdapter {
     @Autowired
     RestTemplate restTemplate;
 
-    @Autowired
-    PokeApiUrl pokeApiUrl;
+    @Value("${poke-api.host}")
+    String pokeApiHost;
 
 
     public Pokemon obtenerPokemon (String nombrePokemon){
 
-        String url = pokeApiUrl+ "/api/v2/pokemon/"+nombrePokemon;
+        String url = pokeApiHost + "/api/v2/pokemon/"+nombrePokemon;
         ResponseEntity<Pokemon> responseEntity = restTemplate.getForEntity(url, Pokemon.class);
         Pokemon pokemon = responseEntity.getBody();
         return pokemon;
--- src/main/java/com/backend/banorte/config/AppConfig.java
+++ src/main/java/com/backend/banorte/config/AppConfig.java
@@ -1,6 +1,7 @@
 package com.backend.banorte.config;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,8 +9,18 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class AppConfig {
 
+    @Value("${poke-api.host}")
+    protected String baseUrl;
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
+
+    @Bean
+    public String pokeApiHost(){
+        return baseUrl;
+    }
+
+
 }
